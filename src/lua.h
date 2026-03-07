fdb4c90967792c4aa2cb8a4af230d1d3c1960201
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-06 22:17:22 -0600

    FEAT: Add a special version to the DW builds

--- src/lua.h
+++ src/lua.h
@@ -16,7 +16,7 @@
 #include "luaconf.h"
 
 
-#define LUA_VERSION	"Lua 5.1"
+#define LUA_VERSION	"Lua 5.1-DW"
 #define LUA_RELEASE	"Lua 5.1.4"
 #define LUA_VERSION_NUM	501
 #define LUA_COPYRIGHT	"Copyright (C) 1994-2008 Lua.org, PUC-Rio"
