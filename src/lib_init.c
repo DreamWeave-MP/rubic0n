0eefc5cf91d7f9ab3a71313cf90ef6483769e714
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-11 10:17:14 -0500

    Revert "FIX: Again"
    This reverts commit 8f03c16bacc67ad8b695426a6574a2c5cdad4851.

--- src/lib_init.c
+++ src/lib_init.c
@@ -47,8 +47,7 @@ LUALIB_API void luaL_openlibs(lua_State *L)
 
   lua_getglobal(L, "table");
   lua_pushcfunction(L, luaopen_ffi);
-  lua_pushstring(L, LUA_FFILIBNAME);
-  lua_call(L, 1, 1);
+  lua_call(L, 0, 1);
   lua_setfield(L, -2, "_ffi");
   lua_pop(L, 1);
 
