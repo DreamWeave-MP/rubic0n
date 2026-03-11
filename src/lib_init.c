014e45e583fc3b3049ea83d53d664eb7f91786b2
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-11 10:17:15 -0500

    Revert "FIX: Forgot to call the returned function"
    This reverts commit acbafbe4875328a609167eae7d4ffbf91a1a1ede.

--- src/lib_init.c
+++ src/lib_init.c
@@ -47,7 +47,6 @@ LUALIB_API void luaL_openlibs(lua_State *L)
 
   lua_getglobal(L, "table");
   lua_pushcfunction(L, luaopen_ffi);
-  lua_call(L, 0, 1);
   lua_setfield(L, -2, "_ffi");
   lua_pop(L, 1);
 
